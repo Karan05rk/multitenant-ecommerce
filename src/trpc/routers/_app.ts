53cbde540161079db03844180663ce28f994c87a
Karan05rk <karan05rk@gmail.com>
2025-07-03 03:37:43 +0530

    22: Reviews

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from '../init';
 
 import { authRouter } from '@/modules/auth/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
+import { reviewsRouter } from '@/modules/reviews/server/procedures';
 import { tenantsRouter } from '@/modules/tenants/server/procedures';
 import { libraryRouter } from '@/modules/library/server/procedures';
 import { checkoutRouter } from '@/modules/checkout/server/procedures';
@@ -11,6 +12,7 @@ import { categoryiesRouter } from '@/modules/categories/server/procedures';
 export const appRouter = createTRPCRouter({
     auth: authRouter,
     tags: tagsRouter,
+    reviews: reviewsRouter,
     tenants: tenantsRouter,
     library: libraryRouter,
     checkout: checkoutRouter,
