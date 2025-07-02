15045c996659b21fbe3eed889730a2abf7b06b14
Karan05rk <karan05rk@gmail.com>
2025-07-02 21:27:14 +0530

    21: Library

--- src/modules/products/ui/components/cart-button.tsx
+++ src/modules/products/ui/components/cart-button.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
@@ -6,10 +8,26 @@ import { useCart } from "@/modules/checkout/hooks/use-cart";
 interface Props {
     tenantSlug: string;
     productId: string;
+    isPurchased?: boolean;
 };
 
-export const CartButton = ({ tenantSlug, productId }: Props) => {
+export const CartButton = ({ tenantSlug, productId, isPurchased }: Props) => {
     const cart = useCart(tenantSlug);
+
+    if (isPurchased) {
+        return (
+            <Button
+                variant="elevated"
+                asChild
+                className="flex-1 font-medium bg-white"
+            >
+                <Link prefetch href={`/library/${productId}`}>
+                    View in Library
+                </Link>
+            </Button>
+        )
+    }
+
     return (
         <Button
             variant="elevated"
